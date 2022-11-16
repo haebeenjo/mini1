e2d0b7f417502d03dd65b5ff6b26187bd351becf
Jo haebeen <haebin1622@naver.com>
2022-11-17 01:06:30 +0900

    v.1.0.1

--- app.py
+++ app.py
@@ -2,35 +2,31 @@ from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
 
 from pymongo import MongoClient
-import certifi
-
-ca = certifi.where()
-client = MongoClient('mongodb+srv://sayapinn:$100doller@cluster0.qv0cojm.mongodb.net/Cluster0?retryWrites=true&w=majority', tlsCAFile=ca)
+client = MongoClient('mongodb+srv://test:sparta@cluster0.hrta48t.mongodb.net/Cluster0?retryWrites=true&w=majority')
 db = client.dbsparta
-
 
 @app.route('/')
 def home():
    return render_template('index.html')
 
-@app.route("/teamintro", methods=["POST"])
-def teamintro_post():
+@app.route("/first_mini", methods=["POST"])
+def first_mini_post():
     name_receive = request.form['name_give']
     comment_receive = request.form['comment_give']
 
     doc = {
-        'name': name_receive,
-        'comment': comment_receive
+        'name':name_receive,
+        'comment':comment_receive
     }
 
-    db.intro.insert_one(doc)
+    db.first_mini.insert_one(doc)
 
-    return jsonify({'msg':'방명록을 남겼습니다.'})
+    return jsonify({'msg':'저장 완료'})
 
-@app.route("/teamintro", methods=["GET"])
-def teamintro_get():
-    comment_list = list(db.intro.find({}, {'_id': False}))
-    return jsonify({'comments':comment_list})
+@app.route("/first_mini", methods=["GET"])
+def first_mini_get():
+    homework_list = list(db.first_mini.find({}, {'_id': False}))
+    return jsonify({'first_mini':homework_list})
 
 if __name__ == '__main__':
    app.run('0.0.0.0', port=5000, debug=True)
